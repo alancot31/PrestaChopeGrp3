3f1bdefc386f67db3c8a7fce0191afd6a5840d7e
alancot31 <77280377+alancot31@users.noreply.github.com>
2021-04-02 15:59:19 +0200

    connexion

---
+++ pages/connexion/connexion.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Titre de la page</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="script.js"></script>
+</head>
+<body>
+
+
+<form action="action.php" method="post">
+    <div>
+        <label for="pseudo">Identifiant :</label>
+        <input type="text" id="pseudo" name="pseudo">
+    </div>
+    <div>
+        <label for="password">Mots De Passe:</label>
+        <input type="password" id="mdp" name="mdp">
+        <p><input type="submit" value="OK"></p>
+    </div>
+</form>
+</body>
+</html>
--- tools/SuperControleur.php
+++ tools/SuperControleur.php
@@ -6,17 +6,17 @@ class SuperControleur
     public static function callPage($page){
         switch ($pages){
             case "connexion" :
-                include_once("pages/connexion/controleurConnexion.php");
+                include_once("pages/connexion/ControleurConnexion.php");
             case"filtres":
                 include_once("pages/filtres/controleurFiltres");
             case"commande":
-                include_once ("pages/commande/controleirCommande");
+                include_once ("pages/commande/controleurCommande");
             case"compte":
                 include_once ("pages/infoCompte/controleurCompte");
             case"contact":
-                include_once ("pages/contact/controleurContact");
+                include_once ("pages/formulaireContacte/controleurContact");
             case"commantaire":
-                include_once ("pages/commantaire/controleurCommande");
+                include_once ("pages/commentaires/controleurCommande");
         }
     }
 }
