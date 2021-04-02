6ebc3464eb675b97bf2d597cb2b44f484909286f
alancot31 <77280377+alancot31@users.noreply.github.com>
2021-04-02 16:07:37 +0200

    connexion

--- pages/connexion/connexion.php
+++ pages/connexion/connexion.php
@@ -3,23 +3,21 @@
 <head>
     <meta charset="utf-8">
     <title>Titre de la page</title>
-    <link rel="stylesheet" href="style.css">
-    <script src="script.js"></script>
+    <link rel="stylesheet" href="">
 </head>
 <body>
 
 
-<form action="action.php" method="post">
+<form action="" method="post">
     <div>
         <label for="pseudo">Identifiant :</label>
         <input type="text" id="pseudo" name="pseudo">
-    </div>
-    <div>
+
         <label for="password">Mots De Passe:</label>
         <input type="password" id="mdp" name="mdp">
+
         <p><input type="submit" value="OK"></p>
     </div>
 </form>
-aa
 </body>
 </html>
